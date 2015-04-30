a47a43d8ad079f4d147cba5dda18b5801371ec36
Tristan Rubadeau <tristan@rubadeau.com>
2015-04-30 11:37:00 -0600

    able to clear all tracks

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
       $("#stopwatch").timer('pause');
       pauseTracks();
       $(this).removeClass("rotate");
-      clearTrack();
     }
     else {
       delayStart();
@@ -19,21 +18,22 @@ function playTrack(track) {
   track.play();
 };
 
+var arrIDs = [];
 function pauseTracks() {
   $.each($('audio'), function(index, track) {
     track.pause();
   });
+    arrIDs.forEach(function(x) {
+    window.clearTimeout(x);
+  })
+  arrIDs = [];
 };
 
 function delayStart() {
-  setTimeout(function(){ playTrack($('#raven').get(0)) }, 100);
-  setTimeout(function(){ playTrack($('#drums1').get(0)) }, 3000);
-  setTimeout(function(){ playTrack($('#didgi1').get(0)) }, 10000);
-  setTimeout(function(){ playTrack($('#bass').get(0)) }, 7000);
-  setTimeout(function(){ playTrack($('#didgi2').get(0)) }, 20000);
-  setTimeout(function(){ playTrack($('#everything').get(0)) }, 30000);
-};
-
-function clearTrack() {
-  clearTimeout();
+  arrIDs.push(setTimeout(function(){ playTrack($('#raven').get(0)) }, 100));
+  arrIDs.push(setTimeout(function(){ playTrack($('#drums1').get(0)) }, 3000));
+  arrIDs.push(setTimeout(function(){ playTrack($('#didgi1').get(0)) }, 10000));
+  arrIDs.push(setTimeout(function(){ playTrack($('#bass').get(0)) }, 7000));
+  arrIDs.push(setTimeout(function(){ playTrack($('#didgi2').get(0)) }, 20000));
+  arrIDs.push(setTimeout(function(){ playTrack($('#everything').get(0)) }, 30000));
 };
